b00a0be3f6bc3baae44ae9371b0ad3e1ecd7b072
b3t0 <b3t0@rhin>
2023-01-06 11:57:56 -0600

    strings replace 3l1t3

--- stringReplace/main.go
+++ stringReplace/main.go
@@ -6,7 +6,40 @@ import (
 )
 
 func main() {
-	s := "Hola, mundo!:"
-	s = strings.Replace(s, "a", "x", -1)
-	fmt.Println(s) // Salida: "Hxlo, mundo!"
+	input := "HEllo, WOrld!"
+	output := ""
+
+	// Iterate over each character in the input string
+	for _, c := range input {
+		// Check if the character should be replaced according to the chart
+		switch c {
+		case 'A':
+			output += "4"
+		case 'B':
+			output += "8"
+		case 'E':
+			output += "3"
+		case 'G':
+			output += "6"
+		case 'I':
+			output += "1"
+		case 'O':
+			output += "0"
+		case 'S':
+			output += "5"
+		case 'T':
+			output += "7"
+		case 'Z':
+			output += "2"
+		default:
+			// If the character is not on the chart, leave it unchanged
+			output += string(c)
+		}
+	}
+
+	// Convert the output string to lowercase
+	output = strings.ToLower(output)
+
+	fmt.Println(output)
+	// Output: h3ll0, w0rld!
 }
