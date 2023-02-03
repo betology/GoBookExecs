c23e6ea8d9c485dfe0ece7b5d4de1f707c5a2364
Beto <b3t0@moon>
2023-02-02 21:30:39 -0600

    simple httpserver

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
+type StringHandler struct {
+	message string
+}
+
+func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, sh.message)
+}
+
 func main() {
-	for _, p := range Products {
-		Printfln("Product: %v, Category: %v, Price: $%.2f",
-			p.Name, p.Category, p.Price)
+	err := http.ListenAndServe(":5000", StringHandler{message: "Hello, World"})
+	if err != nil {
+		Printfln("Error: %v", err.Error())
 	}
 }
