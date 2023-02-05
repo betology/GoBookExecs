74a86c66bcd78fb79601bc5a97e2fb5305095d16
Beto <b3t0@moon>
2023-02-04 22:37:06 -0600

    request fields wrote

--- httpserver/main.go
+++ httpserver/main.go
@@ -10,6 +10,14 @@ type StringHandler struct {
 }
 
 func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	Printfln("Method: %v", request.Method)
+	Printfln("URL: %v", request.URL)
+	Printfln("HTTP Version: %v", request.Proto)
+	Printfln("Host: %v", request.Host)
+	for name, val := range request.Header {
+		Printfln("Header: %v, Value: %v", name, val)
+	}
+	Printfln("---")
 	io.WriteString(writer, sh.message)
 }
 
