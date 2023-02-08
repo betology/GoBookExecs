da22bcbdf91ac57a3c3f22ba728bd1fde312d499
Your Name :) <beto@cypherite>
2023-02-08 16:06:41 -0600

    123 multiple else/if

--- flowcontrol/main.go
+++ flowcontrol/main.go
@@ -7,7 +7,9 @@ func main() {
 
 	if kayakPrice > 500 {
 		fmt.Println("Price is greater tha 500")
-	} else if (kayakPrice < 300) {
-		fmt.Println("Price is less than 300")
+	} else if (kayakPrice < 100) {
+		fmt.Println("Price is less than 100")
+	} else if (kayakPrice > 200 && kayakPrice < 300) {
+		fmt.Println("Price is between 200 and 300")
 	}
 }
