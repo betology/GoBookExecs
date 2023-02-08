e5dc3e58f03d42a1f0fb3a6354c7782343371e68
Your Name :) <beto@cypherite>
2023-02-08 15:01:08 -0600

    120 _else if_

--- flowcontrol/main.go
+++ flowcontrol/main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	kayakPrice := 275.00
 
-	if kayakPrice > 100 {
-		fmt.Println("Price is greater tha 100. Price: ", kayakPrice)
+	if kayakPrice > 500 {
+		fmt.Println("Price is greater tha 500")
+	} else if (kayakPrice < 300) {
+		fmt.Println("Price is less than 300")
 	}
 }
