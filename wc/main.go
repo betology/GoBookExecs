f2ae4a61b92fe30606637039b3efef844d917b55
betology <b3t0@polestar>
2023-06-27 16:29:53 -0600

    Added Command-Line Flags

--- wc/main.go
+++ wc/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsing the flags privided by the user
+	flag.Parse()
 	// Calling the count function ito count the number of words
 	// received from the Standard Imput and printin it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words so we define
+	// the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining a counter
 	wc := 0
