f1433c95653ecab73092776a24212920b05e9ac8
betology <betology@gmail.com>
2023-08-01 20:55:51 -0600

    calculation

---
+++ l2/mars.go
@@ -0,0 +1,11 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(105 * 0.3783)
+	fmt.Print(" Kg., and I would be ")
+	fmt.Print(44 * 365 / 687)
+	fmt.Print(" years old.")
+}
